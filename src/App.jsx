7eff352503451bf4bd1281d20d82b16833319af7
DoriaFabio <doriafabio2001@gmail.com>
2024-12-17 22:16:14 +0000

    do scaffolding

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+import './index.css'
+import Header from './components/Header'
+import Form from './components/Form'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header />
+      <Form />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+function Header() {
+    return <h1>Form</h1>
+}
+
+export default Header;
